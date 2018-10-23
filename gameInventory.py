c0d99754cf82358eb54e7919088bbd23ec25afba
magg666 <mw23127@gmail.com>
2018-10-23 12:35:21 +0200

    Added code

--- gameInventory.py
+++ gameInventory.py
@@ -1,38 +1,75 @@
-# This is the file where you must work. Write code in the functions, create new functions, 
+# This is the file where you must work. Write code in the functions, create new functions,
 # so they work according to the specification
-
+import csv
 # Displays the inventory.
+
+
 def display_inventory(inventory):
-    pass
+    print("Inventory:")
+    for key, value in sorted(inventory.items()):
+        print(value, key)
+        total = sum(inventory.values())
+        print("Total numbers of items: ", total)
 
 
 # Adds to the inventory dictionary a list of items from added_items.
 def add_to_inventory(inventory, added_items):
-    pass
+    for item in added_items:
+        if item in inventory:
+            inventory[item] += 1
+        if item not in inventory:
+            inventory[item] = 1
 
 
-# Takes your inventory and displays it in a well-organized table with 
+# Takes your inventory and displays it in a well-organized table with
 # each column right-justified. The input argument is an order parameter (string)
 # which works as the following:
 # - None (by default) means the table is unordered
-# - "count,desc" means the table is ordered by count (of items in the inventory) 
+# - "count,desc" means the table is ordered by count (of items in the inventory)
 #   in descending order
 # - "count,asc" means the table is ordered by count in ascending order
 def print_table(inventory, order=None):
-    pass
+    a = (max(len(key) for key in inventory.keys()))+2
+    b = (max(len(str(value)) for value in inventory.values()))+4
+    print("\nInventory:")
+    print('{:>{b}}''{:>{a}}'.format("count", "item name", b=b, a=a))
+    print("-" * (a+b))
+    if order == "count,desc":
+        for value in sorted(inventory, key=inventory.get, reverse=True):
+            print('{:>{b}}''{:>{a}}'.format(inventory[value], value, b=b, a=a))
+    elif order == "count,asc":
+        for value in sorted(inventory, key=inventory.get):
+            print('{:>{b}}''{:>{a}}'.format(inventory[value], value, b=b, a=a))
+    elif order is None:
+        for key, value in sorted(inventory.items()):
+            print('{:>{b}}''{:>{a}}'.format(value, key, b=b, a=a))
+    else:
+        exit()
+    print("-" * (a+b))
+    total = sum(inventory.values())
+    print("Total numbers of items: ", total)
 
 
 # Imports new inventory items from a file
-# The filename comes as an argument, but by default it's 
+# The filename comes as an argument, but by default it's
 # "import_inventory.csv". The import automatically merges items by name.
 # The file format is plain text with comma separated values (CSV).
 def import_inventory(inventory, filename="import_inventory.csv"):
-    pass
+    with open(filename, 'r') as file:
+        for line in file.readlines():
+            file_treasure = line.split(",")
+            for item in file_treasure:
+                if item in inventory:
+                    inventory[item] += 1
+                if item not in inventory:
+                    inventory[item] = 1
 
 
 # Exports the inventory into a .csv file.
 # if the filename argument is None it creates and overwrites a file
-# called "export_inventory.csv". The file format is the same plain text 
+# called "export_inventory.csv". The file format is the same plain text
 # with comma separated values (CSV).
 def export_inventory(inventory, filename="export_inventory.csv"):
-    pass
+    with open(filename, 'w') as file:
+        for value in inventory:
+            print((value + ',') * inventory[value])
